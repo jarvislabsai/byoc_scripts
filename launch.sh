0ca9912c30ac9348d272f078f0168ba5e643113e
Vishnu Subramanian <johnfedrickenator@gmail.com>
2021-10-28 11:42:13 +0530

    ch

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
-SSH_KEY=hello
-bash -c 'echo "$SSH_KEY" | tee -a ~/.ssh/authorized_keys' && \
-    chmod 600 ~/.ssh/authorized_keys && \
-    bash -c 'echo "$SHELL"'
-
-
+apt install openssh-server -y automatically
+mkdir -p ~/.ssh
+SSH_KEY=$1
+echo $SSH_KEY
+echo $SSH_KEY | tee -a ~/.ssh/authorized_keys && \
+    chmod 600 ~/.ssh/authorized_keys
