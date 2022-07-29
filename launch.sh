7f5bd0ca7a6de5d34e78f01f08a450ea93b924a2
vishnu kumar <vishnu.techsource@gmail.com>
2022-07-29 10:32:25 +0530

    modyfing the code for removing input for script

--- launch.sh
+++ launch.sh
@@ -1,9 +1,5 @@
 DEBIAN_FRONTEND=noninteractive apt install openssh-server -y 
 mkdir -p ~/.ssh
-SSH_KEY=$1
-echo -e $SSH_KEY
-echo -e $SSH_KEY | tee -a ~/.ssh/authorized_keys && \
-    chmod 600 ~/.ssh/authorized_keys
 service ssh restart
 echo "cd /home" >> ~/.bashrc
 echo "export HOME=/home/" >> ~/.bashrc
