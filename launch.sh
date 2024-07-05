397bd9199c2f4a359977c60599d108b9979e95ba
coding-medjey <spvaran123@gmail.com>
2024-07-05 11:08:50 +0530

    added deps

--- launch.sh
+++ launch.sh
@@ -1,6 +1,18 @@
+cd /root
+apt update
+apt-get install -y git wget vim
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
+rm -rf Miniconda3-latest-Linux-x86_64.sh 
 DEBIAN_FRONTEND=noninteractive apt install openssh-server -y 
 mkdir -p ~/.ssh
 service ssh restart
-echo "cd /home" >> ~/.bashrc
-echo "export HOME=/home/" >> ~/.bashrc
+echo "cd /root" >> ~/.bashrc
+echo "export HOME=/root/" >> ~/.bashrc
+echo "PATH='/root/miniconda3/bin:/root/miniconda3/condabin:/root/miniconda3/bin:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:~/.local/bin'" >> ~/.bashrc
+source .bashrc
+conda init && source .bashrc
+pip install jupyterlab notebook ipywidgets
 export SHELL="/bin/bash"
+env HOME=/root/ jupyter lab --ip=0.0.0.0 --NotebookApp.token=$TOKEN  --allow-root --port 8889
+rm -rf launch.sh
